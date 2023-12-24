46cdafe79cb4a8025dde5c197eddf20ce4499f8b
nsantacruz <noahssantacruz@gmail.com>
2023-12-24 14:45:41 +0200

    test: add test for Derashat Shabbat

--- tests/exporter_tests.py
+++ tests/exporter_tests.py
@@ -13,7 +13,7 @@ def simple_index_exporter():
 @pytest.fixture()
 def all_version_index_exporter():
     i = library.get_index("Job")
-    return IndexExporter(i, included_all_versions=True)
+    return IndexExporter(i, include_all_versions=True)
 
 
 def test_num_chunks(simple_index_exporter, all_version_index_exporter):
@@ -24,6 +24,7 @@ def test_num_chunks(simple_index_exporter, all_version_index_exporter):
 @pytest.mark.parametrize(('title', 'tref', 'include_all_versions'), [
     ['Job', 'Job 17', False],
     ['Job', 'Job 17', True],
+    ['Derashat Shabbat HaGadol', 'Derashat Shabbat HaGadol 1', False],
 ])
 def test_section_data(title, tref, include_all_versions):
     index = library.get_index(title)
@@ -31,7 +32,8 @@ def test_section_data(title, tref, include_all_versions):
     oref = model.Ref(tref)
     num_versions = len(index.versionSet()) if include_all_versions else 2
     num_segments = len(oref.all_segment_refs())
-    text_by_version, metadata = exporter.section_data(oref, {})
+    text_by_version, metadata = exporter.section_data(oref)
     assert len(text_by_version) == num_versions
     for key, value in text_by_version.items():
         assert len(value) <= num_segments
+        print(key)
