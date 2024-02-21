7bf1eb7b40d549fe9d83df954028fa8ccf91fedf
nsantacruz <noahssantacruz@gmail.com>
2024-02-21 13:00:47 +0200

    test: dont use import * in tests

--- tests/exporter_tests.py
+++ tests/exporter_tests.py
@@ -1,19 +1,19 @@
 import pytest
-from JsonExporterForIOS import *
-from sefaria.model.text import library
+import JsonExporterForIOS as jefi
+from sefaria.model.text import library, Ref
 
-model.library.rebuild_toc()
+library.rebuild_toc()
 
 @pytest.fixture()
 def simple_index_exporter():
     i = library.get_index("Job")
-    return IndexExporter(i)
+    return jefi.IndexExporter(i)
 
 
 @pytest.fixture()
 def all_version_index_exporter():
     i = library.get_index("Job")
-    return IndexExporter(i, include_all_versions=True)
+    return jefi.IndexExporter(i, include_all_versions=True)
 
 
 def test_num_chunks(simple_index_exporter, all_version_index_exporter):
@@ -25,11 +25,12 @@ def test_num_chunks(simple_index_exporter, all_version_index_exporter):
     ['Job', 'Job 17', False, 2],
     ['Job', 'Job 17', True, 15],
     ['Derashat Shabbat HaGadol', 'Derashat Shabbat HaGadol 1', False, 1],
+    ['Malbim Beur Hamilot on Nahum', 'Malbim Beur Hamilot on Nahum 1:1', True, 1],
 ])
 def test_section_data(title, tref, include_all_versions, expected_num_versions):
     index = library.get_index(title)
-    exporter = IndexExporter(index, include_all_versions)
-    oref = model.Ref(tref)
+    exporter = jefi.IndexExporter(index, include_all_versions)
+    oref = Ref(tref)
     num_versions = len(index.versionSet()) if expected_num_versions is None else expected_num_versions
     num_segments = len(oref.all_segment_refs())
     text_by_version, metadata = exporter.section_data(oref)
@@ -46,8 +47,8 @@ def test_merged_chunk():
     Test that top priority version title is chosen for a merged chunk
     """
     index = library.get_index("Berakhot")
-    exporter = IndexExporter(index, False)
-    text_by_version, metadata = exporter.section_data(model.Ref("Berakhot 2a"))
+    exporter = jefi.IndexExporter(index, False)
+    text_by_version, metadata = exporter.section_data(Ref("Berakhot 2a"))
     assert ('William Davidson Edition - English', 'en') in text_by_version
 
 
@@ -56,9 +57,8 @@ def test_remove_empty_versions():
     Test that we remove empty versions from a section
     """
     index = library.get_index("Exodus")
-    exporter = IndexExporter(index, True)
-    text_by_version, metadata = exporter.section_data(model.Ref("Exodus 10"))
+    exporter = jefi.IndexExporter(index, True)
+    text_by_version, metadata = exporter.section_data(Ref("Exodus 10"))
     test_vtitle, test_lang = 'Sefaria Community Translation', 'en'
     assert (test_vtitle, test_lang) not in text_by_version
     assert next((v for v in metadata['versions'] if v['versionTitle'] == test_vtitle and v['language'] == test_lang), None) is None
-
